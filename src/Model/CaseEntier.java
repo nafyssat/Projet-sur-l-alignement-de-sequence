0d9fe34e38658e67e1ca53271a1924031b97b540
Mounia Mkouboi <mkouboi.mounia@outlook.fr>
2022-02-15 14:40:02 +0100

    case entier

--- src/Model/CaseEntier.java
+++ src/Model/CaseEntier.java
@@ -1,5 +1,14 @@
 package Model;
 
 public class CaseEntier extends Case{
+    private int valeur;
+
+    public CaseEntier(int val){
+        this.valeur= val;
+    }
+
+    public int getValeur(){
+        return this.valeur;
+    }
 
 }
