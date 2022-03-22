4bdff817d2271b6a6b762d704fd69c4cafcdd6ea
montassar <montassarbenabdallah169@gmail.com>
2022-03-22 04:35:21 +0100

    Alignement 1ere partie

---
+++ src/Vue/AlignementPanel.java
@@ -0,0 +1,15 @@
+package Vue;
+
+import Controler.MainWindowControler;
+import Model.Utilisateur;
+
+import javax.swing.*;
+
+public class AlignementPanel {
+    private Utilisateur user=new Utilisateur();
+    private MainWindowControler controlleur=new MainWindowControler();
+
+    public JPanel Align(String a, String b, int c, int d, int e){
+        
+    }
+}
--- src/Vue/MainWindowPanel.java
+++ src/Vue/MainWindowPanel.java
@@ -26,29 +26,26 @@ public class MainWindowPanel extends JFrame {
     JButton OptimisePath=new JButton("Compute Optimal Alignment");
     SpinnerModel modelA = new SpinnerNumberModel(
             1, //valeur initiale
-            -100, //valeur minimum
-            100, //valeur maximum
+            -500, //valeur minimum
+            500, //valeur maximum
             1 //pas
     );
     SpinnerModel modelB = new SpinnerNumberModel(
             -1, //valeur initiale
-            -100, //valeur minimum
-            100, //valeur maximum
+            -500, //valeur minimum
+            500, //valeur maximum
             1 //pas
     );
     SpinnerModel modelC = new SpinnerNumberModel(
             -2, //valeur initiale
-            -100, //valeur minimum
-            100, //valeur maximum
+            -500, //valeur minimum
+            500, //valeur maximum
             1 //pas
     );
 
      public MainWindowPanel(){
-        this.setTitle("Global Sequence Alignement");
-        this.pack();
-        this.setDefaultLookAndFeelDecorated(true);
-        this.setExtendedState(this.MAXIMIZED_BOTH);
-        this.setResizable(false);
+         /*JPanel align=new JPanel();
+         align.setBorder(BorderFactory.createTitledBorder("Alignement"));
          JSpinner a = new JSpinner(modelA);
          JSpinner b= new JSpinner(modelB);
          JSpinner c = new JSpinner(modelC);
@@ -76,6 +73,93 @@ public class MainWindowPanel extends JFrame {
          this.mainPanel.add(OptimisePath);
          this.mainPanel.setPreferredSize(new Dimension(400,200));
          this.add(mainPanel,BorderLayout.WEST);
+         this.mainPanel.setBorder(BorderFactory.createTitledBorder("Paramètres"));
+         JLabel al=new JLabel("Score=3");
+         align.add(al);
+         align.setPreferredSize(new Dimension(200,200));
+         this.mainContainer.setLayout(new GridBagLayout());
+         GridBagConstraints N=new GridBagConstraints();
+         JPanel borderLayoutPanel=new JPanel(new BorderLayout());
+         JPanel gridBagLayoutPanel = new JPanel(new GridBagLayout());
+         borderLayoutPanel.add(gridBagLayoutPanel, BorderLayout.NORTH);
+         mainContainer.add(borderLayoutPanel);
+         N.fill=GridBagConstraints.HORIZONTAL;
+         N.gridx=0;
+         N.gridy=0;
+         //mainPanel.setBackground(Color.LIGHT_GRAY);
+         mainContainer.add(mainPanel,N);
+         this.getRootPane().setBorder(BorderFactory.createMatteBorder(4, 4,4,4, Color.GREEN));
+         N.weightx=1.0;
+         N.gridx=1;
+         N.gridy=0;
+         mainContainer.add(align,N);
+         N.weightx = 1;
+         N.weighty = 1;
+
+         mainContainer.add(new JLabel(" "), N);
+         JPanel tr=new JPanel();
+         N.gridx=1;
+         N.gridy=0;
+         tr.setPreferredSize(new Dimension(400,400));
+         mainContainer.add(tr,N);
+         OptimisePath.addActionListener(new ActionListener() {
+             public void actionPerformed(ActionEvent e) {
+
+                 if (user.sequence(seq1.getText()) == true && seq1.getText().length() <= 20  //Ici je n'ai pas trouvé une méthode
+                         && user.sequence(seq2.getText()) == true && seq2.getText().length() <= 20 //Pour appeler la méthode seqeunceValide()
+                 ) {
+
+
+                     String s1=seq1.getText().toUpperCase();
+                     String s2= seq2.getText().toUpperCase();
+                     //�a permet de tester plusieurs sequence sur le meme fenetre sans avoir a rexecuter
+                     N.gridx=0;
+                     N.gridy=1;
+                     ajout.removeAll();
+                     ajout.revalidate();
+                     ajout.repaint();
+                     ajout.setLayout(new BorderLayout());
+                     ajout.add(v.init(s1,s2, (int) a.getValue(), (int) b.getValue(), (int) c.getValue()));
+                     mainContainer.add(ajout,N);            //Dans ce cas on affiche la matrice
+                     //ajout.setVisible(true);
+                 }
+             }
+         });*/
+
+         this.setTitle("Global Sequence Alignement");
+        this.pack();
+        this.setDefaultLookAndFeelDecorated(true);
+        this.setExtendedState(this.MAXIMIZED_BOTH);
+        this.setResizable(false);
+
+        JSpinner a = new JSpinner(modelA);
+         JSpinner b= new JSpinner(modelB);
+         JSpinner c = new JSpinner(modelC);
+         a.setPreferredSize(new Dimension(103,20));
+         b.setPreferredSize(new Dimension(103,20));
+         c.setPreferredSize(new Dimension(103,20));
+         match.setPreferredSize(new Dimension(103,30));
+         mismatch.setPreferredSize(new Dimension(103,30));
+         gap.setPreferredSize(new Dimension(103,30));
+         CustomPath.setPreferredSize(new Dimension(157,20));
+         ClearPath.setPreferredSize(new Dimension(157,20));
+         OptimisePath.setPreferredSize(new Dimension(320,20));
+         this.mainPanel.add(seq1Label);
+         this.mainPanel.add(seq1);
+         this.mainPanel.add(seq2Label);
+         this.mainPanel.add(seq2);
+         this.mainPanel.add(match);
+         this.mainPanel.add(mismatch);
+         this.mainPanel.add(gap);
+         this.mainPanel.add(a);
+         this.mainPanel.add(b);
+         this.mainPanel.add(c);
+         this.mainPanel.add(CustomPath);
+         this.mainPanel.add(ClearPath);
+         this.mainPanel.add(OptimisePath);
+         this.mainPanel.setPreferredSize(new Dimension(400,200));
+         this.add(mainPanel,BorderLayout.WEST);
+         this.mainPanel.setBorder(BorderFactory.createTitledBorder("Paramètres"));
          this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
          mainContainer.setLayout((new BorderLayout(8,6)));
          mainContainer.setBackground(Color.YELLOW);
@@ -100,7 +184,7 @@ public class MainWindowPanel extends JFrame {
                 	
                 	String s1=seq1.getText().toUpperCase();
                 	String s2= seq2.getText().toUpperCase();
-                	//�a permet de tester plusieurs sequence sur le meme fenetre sans avoir a rexecuter
+                	//�a permet de tester plusieurs sequence sur le meme fenetre sans avoir a rexecuter
                 	ajout.removeAll();
                 	ajout.revalidate();
                 	ajout.repaint();
