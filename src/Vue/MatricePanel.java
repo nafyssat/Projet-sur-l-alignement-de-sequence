db15df2ae7600db3d08de5debef3adf5ac4621ff
montassar <montassarbenabdallah169@gmail.com>
2022-04-29 00:41:45 +0200

    Custom path button #1

--- src/Vue/MatricePanel.java
+++ src/Vue/MatricePanel.java
@@ -21,10 +21,19 @@ import Controler.*;
 public class MatricePanel {
     private Utilisateur user = new Utilisateur();
     private MainWindowControler Controlleur = new MainWindowControler();
-    private JButton [][] matrice;
+    private JButton [][] res;
     private JPanel droite=new JPanel();
     private String [] alignement=new String [3]; //Cette attribut est pour l'affichage de l'alignement
-    private boolean custom;
+    int count=0;
+     int px=-1;
+     int py=-1;
+     String seq1="";
+     String seq2="";
+     int Score=0;
+
+    public MatricePanel(){
+
+    }
    
 
 
@@ -40,7 +49,7 @@ public class MatricePanel {
      */
 
     public JPanel init(String a, String b, int c, int d, int e) { 
-        JButton [][] res = new JButton[a.length() + 2][b.length() + 2];
+        JButton[][] res = new JButton[a.length() + 2][b.length() + 2];
 
         droite.setPreferredSize(new Dimension(20 * (a.length() + 2), 20 * (b.length() + 2)));
 
@@ -181,15 +190,12 @@ public class MatricePanel {
         res[1][1].setBackground(Color.green);
 
 
-        this.matrice=res;
+       // this.matrice=res;
         return droite;
     }
 
 
     public JPanel initClear(String a, String b, int c, int d, int e) {
-
-
-
         JPanel droite = new JPanel();
         JButton[][] res = new JButton[a.length() + 2][b.length() + 2];
 
@@ -347,18 +353,215 @@ public class MatricePanel {
 
     //Custom path
 
+    public JPanel initCustom(String a, String b, int c, int d, int e){
+        JPanel droite = new JPanel();
+         res = new JButton[a.length() + 2][b.length() + 2];
+
+        droite.setPreferredSize(new Dimension(20*(a.length()+2),20*(b.length()+2)));
+
+        droite.setLayout(new GridLayout(a.length()+2,b.length()+2));
+
+        for(int i=0;i<a.length()+2;i++){
+            for(int j=0;j<b.length()+2;j++){
+                res[i][j]=new JButton("  ");
+                res[i][j].setForeground(Color.BLACK);
+                res[i][j].setBackground(Color.white);
+                //On initialise chaque JButton à " " pour éviter par la suite
+                //d'avoir des problème de NullPointerException
+
+            }
+        }
+
+        for (int i = 2; i < a.length() + 2; i++) {
+
+            res[i][0].setText(" "+a.charAt(i-2)); //ON complète la première colonne
+            res[i][0].setForeground(Color.white);
+            res[i][0].setBackground(Color.lightGray);
 
 
-    /**
-     *
-     * @param m matrice
-     * @param a sequence 1
-     * @param b sequence 2
-     * Cette fonction va remettre seulement les Jboutons de la matrices à la couleur blanches (seulement les JButtons
-     * qui ont des entiers
-     *
-     */
-    public void clearMatrice( String a, String b) {
+            res[i][0].setBackground(Color.lightGray);
+            res[i][0].setText(" "+String.valueOf(a.charAt(i-2)).toUpperCase()); //ON complète la première colonne
+
+
+        }
+
+        for (int i = 2; i < b.length() + 2; i++) {
+
+            res[0][i].setText(" "+b.charAt(i-2)); //On complète la première ligne
+            res[0][i].setForeground(Color.white);
+            res[0][i].setBackground(Color.lightGray);
+
+            res[0][i].setBackground(Color.lightGray);
+            res[0][i].setText(" "+String.valueOf(b.charAt(i-2)).toUpperCase()); //On complète la première ligne
+
+
+        }
+
+        for(int i=1;i<a.length()+2;i++){
+            for(int j=1;j<b.length()+2;j++){
+
+                //Ici nous devons appeler cette méthode pour calculer les valeur de chaque cases de
+                //la matrice.
+
+                Case[][] g=this.Controlleur.getGrillePanel().initialiser_Grille_Interface(a, b, c, d, e);
+                res[i][j].setText(" "+g[i][j].getValeur());
+                int finalI = i;
+                int finalJ = j;
+
+               // this.res[i][j].addMouseListener(new MouseAdapter() {
+
+                   // @Override
+                   // public void mouseClicked(MouseEvent x) {
+                    //    if (isValid(finalI,finalJ,c,d,e) ) {
+                           /* JTextArea labelArea = new JTextArea("\n" + seq1 + "\n" + seq2 + "\n" + "\n" + "Score:" + Score);
+                            labelArea.setFont(labelArea.getFont().deriveFont(Font.BOLD));
+                            labelArea.setEditable(false);
+                            labelArea.setOpaque(false);
+                            alignement.removeAll();
+                            alignement.add(labelArea, BorderLayout.WEST);*/
+                            //alignement.setBorder(BorderFactory.createTitledBorder("Alignement"));
+                            //alignement.setPreferredSize(new Dimension((largeur - 596), 131));
+                            //System.out.println("oui");
+                    //        res[finalI][finalJ].setBackground(Color.red);
+                 //       }
+                //    }
+             //   });
+
+
+            }}
+        for(int i=2;i<a.length()+2;i++) {
+            for(int j=2;j<b.length()+2;j++) {
+
+
+                //fenetre expliquant la calcul du score
+
+                String [] s =this.Controlleur.getGrillePanel().affCalVal(i,j,c,d,e);
+                JFrame fr = new JFrame();
+                fr.setLayout(new FlowLayout(FlowLayout.CENTER));
+                fr.setSize(new Dimension(360,188));
+                for(int v=0;v<4;v++){
+                    JPanel w=new JPanel();
+                    w.setPreferredSize(new Dimension(170,70));
+                    w.setBackground(Color.lightGray);
+                    JLabel l=new JLabel(s[v]);
+                    l.setFont(new Font("Calibri", Font.BOLD,10));
+                    w.add(l,BorderLayout.CENTER);
+                    fr.add(w,BorderLayout.CENTER);
+                }
+                fr.setVisible(false);
+
+
+            }
+        }
+        for(int k=1;k<a.length()+2;k++) {
+            res[k][1].setBackground(Color.GRAY);
+
+        }
+        for(int k=1;k<b.length()+2;k++) {
+            res[1][k].setBackground(Color.GRAY);
+
+        }
+
+
+
+
+
+
+        res[a.length()+1][b.length()+1].setBackground(Color.white);
+        res[1][1].setBackground(Color.white);
+
+
+
+
+        //backTracking
+        int f=a.length()+1;
+        int h=b.length()+1;
+
+
+        while(f>=1 && h>=1) {
+            //on recupere la position du case a colorie
+
+
+            res[f][h].setBackground(Color.white);
+            int [] t  = this.Controlleur.getGrillePanel().caseAColorie(f, h, c, d, e);
+            f=t[0];
+            h=t[1];
+            if(f==1 && h==1) {
+                res[f][h].setBackground(Color.white);
+                break;
+            }
+
+
+
+
+        }
+
+
+        // ajouter la table au frame
+
+        for(int i=0;i<a.length()+2;i++) {
+            for (int j = 0; j < b.length() + 2; j++) {
+                if(i==0 || j==0){
+                    res[i][j].setFont(new Font("Italic", Font.BOLD, 13));
+                }else{
+                    res[i][j].setFont(new Font("Calibri", Font.BOLD, 8));
+                }
+                droite.add(res[i][j]);
+            }
+        }
+        res[1][1].setBackground(Color.white);
+
+
+
+
+
+        return droite;
+
+    }
+
+    public boolean isValid(int i,int j,int c,int d,int e) {
+        if(count==0 || (i==px-1 && j==py-1) || (i==px && j==py-1)
+                || (i==px-1 && j==py)){
+            if(this.px!=-1 && this.py!=-1){
+                if(i==px-1 && j==py-1){
+                    this.seq1=res[i+1][0].getText()+seq1;
+                    this.seq2=res[0][j+1].getText()+seq2;
+                    if(res[i+1][0].getText().equals(res[0][j+1].getText())){
+                        this.Score+=c;
+                    }else{
+                        this.Score+=e;
+                    }
+                }
+                else if(i==px-1 && j==py){
+                    this.seq1=res[i+1][0].getText()+seq1;
+                    this.seq2="- "+seq2;
+                    this.Score+=e;
+                }else{
+                    this.seq1="- "+seq1;
+                    this.seq2=res[0][j+1].getText()+seq2;
+                    this.Score+=e;
+                }
+            }
+            count++;
+            this.px=i;
+            this.py=j;
+
+            return true;
+        }
+        return false;
+    }
+
+    public JButton[][]getMatrice(){return this.res;}
+
+
+
+
+
+
+
+
+
+   /* public void clearMatrice( String a, String b) {
         for(int i=2;i<a.length()+2;i++) {
             for(int j=2;j<b.length()+2;j++) {
                 
@@ -366,14 +569,7 @@ public class MatricePanel {
             }
         }
     }
-    /**
-     *
-     * @param m
-     * @param i index de la matrice à la position i(ligne)
-     * @param j index de la matrice à la position j (colonne)
-     * Ici on veut vérifier si dans les Jbutton autours de celui de l'index i, j . On veut
-     * vérifier s'il y'en a qu'ils sont de couleur jaunes, pour que le chemin puisse être cyclique.
-     */
+
     public boolean calcule_adjacence( int i, int j ) {
         matrice[i][j].addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
@@ -416,15 +612,7 @@ public class MatricePanel {
     }
 
 
-    /**
-     *
-     * @param a sequence 1
-     * @param b sequence 2
-     * @param matrice
-     * Nous allons choisir un chemin en partant de la fin.
-     * C'est pourquoi nous avons mis directement la case matrice[a.length()+1][b.length()+1] à 
-     * la couleur jaune, pour qu'on puisse suivre le chemin à partir de ce dernier.
-     */
+
    public JPanel custom_path(String a, String b,  int ma, int mi, int gap){
 
        this.init(a, b, ma, mi, gap);
@@ -450,7 +638,7 @@ public class MatricePanel {
         }
         return droite;
         
-    }
+    }*/
 
     /**
      * 
@@ -465,7 +653,7 @@ public class MatricePanel {
      * manière différentes par la suite.
      */
 
-    public String [] getAlign_custom(String a, String b) {
+    /*public String [] getAlign_custom(String a, String b) {
         int i=a.length()+1;
         int j=b.length()+1;
         while(i>=2 && j>=2){
@@ -504,6 +692,6 @@ public class MatricePanel {
         alignement[2]="Score: "+this.matrice[a.length()+1][b.length()+1];
        
         return this.alignement;
-    }
+    }*/
 
 }
