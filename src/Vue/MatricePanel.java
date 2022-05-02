adc3e7cfa401f3cf2ea59d87af6eef033bc440bc
montassar <montassarbenabdallah169@gmail.com>
2022-05-02 17:09:35 +0200

    Update customPath #1

--- src/Vue/MatricePanel.java
+++ src/Vue/MatricePanel.java
@@ -281,128 +281,32 @@ public class MatricePanel {
 
     public JPanel initCustom(String a, String b, int c, int d, int e){
         JPanel droite = new JPanel();
-         res = new JButton[a.length() + 2][b.length() + 2];
-
-        droite.setPreferredSize(new Dimension(20*(a.length()+2),20*(b.length()+2)));
-
+        res = new JButton[a.length() + 2][b.length() + 2];
         droite.setLayout(new GridLayout(a.length()+2,b.length()+2));
-
+        //TODO: initiliaser les JButtons
         for(int i=0;i<a.length()+2;i++){
             for(int j=0;j<b.length()+2;j++){
                 res[i][j]=new JButton("  ");
-                res[i][j].setForeground(Color.BLACK);
-                res[i][j].setBackground(Color.white);
-                //On initialise chaque JButton à " " pour éviter par la suite
-                //d'avoir des problème de NullPointerException
-
             }
         }
-
+        //TODO: remplir la 1ère colonne
         for (int i = 2; i < a.length() + 2; i++) {
-
-            res[i][0].setText(" "+a.charAt(i-2)); //ON complète la première colonne
-            res[i][0].setForeground(Color.white);
-            res[i][0].setBackground(Color.lightGray);
-
-
-            res[i][0].setBackground(Color.lightGray);
-            res[i][0].setText(" "+String.valueOf(a.charAt(i-2)).toUpperCase()); //ON complète la première colonne
-
-
+            res[i][0].setText(" "+a.charAt(i-2));
+            res[i][0].setText(" "+String.valueOf(a.charAt(i-2)).toUpperCase());
         }
-
+        //TODO: remplirla 1ère ligne
         for (int i = 2; i < b.length() + 2; i++) {
-
-            res[0][i].setText(" "+b.charAt(i-2)); //On complète la première ligne
-            res[0][i].setForeground(Color.white);
-            res[0][i].setBackground(Color.lightGray);
-
-            res[0][i].setBackground(Color.lightGray);
+            res[0][i].setText(" "+b.charAt(i-2));
             res[0][i].setText(" "+String.valueOf(b.charAt(i-2)).toUpperCase()); //On complète la première ligne
-
-
         }
-
-        for(int i=1;i<a.length()+2;i++){
-            for(int j=1;j<b.length()+2;j++){
-
-                //Ici nous devons appeler cette méthode pour calculer les valeur de chaque cases de
-                //la matrice.
-
-                Case[][] g=this.Controlleur.getGrillePanel().initialiser_Grille_Interface(a, b, c, d, e);
-                res[i][j].setText(" "+g[i][j].getValeur());
-
-            }}
-        for(int i=2;i<a.length()+2;i++) {
-            for(int j=2;j<b.length()+2;j++) {
-
-
-                //fenetre expliquant la calcul du score
-
-                String [] s =this.Controlleur.getGrillePanel().affCalVal(i,j,c,d,e);
-                JFrame fr = new JFrame();
-                fr.setLayout(new FlowLayout(FlowLayout.CENTER));
-                fr.setSize(new Dimension(360,188));
-                for(int v=0;v<4;v++){
-                    JPanel w=new JPanel();
-                    w.setPreferredSize(new Dimension(170,70));
-                    w.setBackground(Color.lightGray);
-                    JLabel l=new JLabel(s[v]);
-                    l.setFont(new Font("Calibri", Font.BOLD,10));
-                    w.add(l,BorderLayout.CENTER);
-                    fr.add(w,BorderLayout.CENTER);
-                }
-                fr.setVisible(false);
-
-
+        //TODO: remplir le reste de la matrice
+        for(int i=1;i<a.length()+2;i++) {
+            for (int j = 1; j < b.length() + 2; j++) {
+                Case[][] g = this.Controlleur.getGrillePanel().initialiser_Grille_Interface(a, b, c, d, e);
+                res[i][j].setText(" " + g[i][j].getValeur());
             }
         }
-        for(int k=1;k<a.length()+2;k++) {
-            res[k][1].setBackground(Color.GRAY);
-
-        }
-        for(int k=1;k<b.length()+2;k++) {
-            res[1][k].setBackground(Color.GRAY);
-
-        }
-
-
-
-
-
-
-        res[a.length()+1][b.length()+1].setBackground(Color.white);
-        res[1][1].setBackground(Color.white);
-
-
-
-
-        //backTracking
-        int f=a.length()+1;
-        int h=b.length()+1;
-
-
-        while(f>=1 && h>=1) {
-            //on recupere la position du case a colorie
-
-
-            res[f][h].setBackground(Color.white);
-            int [] t  = this.Controlleur.getGrillePanel().caseAColorie(f, h, c, d, e);
-            f=t[0];
-            h=t[1];
-            if(f==1 && h==1) {
-                res[f][h].setBackground(Color.white);
-                break;
-            }
-
-
-
-
-        }
-
-
-        // ajouter la table au frame
-
+        //TODO ajouter la table au Jpanel
         for(int i=0;i<a.length()+2;i++) {
             for (int j = 0; j < b.length() + 2; j++) {
                 if(i==0 || j==0){
@@ -413,14 +317,25 @@ public class MatricePanel {
                 droite.add(res[i][j]);
             }
         }
-        res[1][1].setBackground(Color.white);
+        //TODO: changer les couleurs
+        for(int m=2;m<a.length()+2;m++){
+            for(int n=2;n<b.length()+2;n++){
+                res[m][1].setBackground(Color.LIGHT_GRAY);
+                res[1][n].setBackground(Color.LIGHT_GRAY);
+                res[0][n].setBackground(new Color(230, 252, 237));
+                res[0][n].setForeground(new Color(203, 235, 247));
+                res[m][0].setBackground(new Color(230, 252, 237));
+                res[m][0].setForeground(new Color(203, 235, 247));
+                res[m][n].setBackground(Color.WHITE);
+                res[0][0].setBackground(Color.WHITE);
+                res[1][0].setBackground(Color.WHITE);
+                res[0][1].setBackground(Color.WHITE);
+                res[1][1].setBackground(Color.LIGHT_GRAY);
 
 
-
-
-
+            }
+        }
         return droite;
-
     }
 
     public boolean isValid(int i,int j,int c,int d,int e) {
@@ -452,23 +367,6 @@ public class MatricePanel {
 
             return true;
         }
-
-	//Ajout fenetre d'erreur dans le cas où on appuiera sur un mauvais bouton
-	JFrame fr = new JFrame();
-        fr.setLayout(new FlowLayout(FlowLayout.CENTER));
-        fr.setSize(new Dimension(300,120));
-        JPanel w=new JPanel();
-        w.setPreferredSize(new Dimension(300,120));
-        w.setBackground(Color.lightGray);
-        JLabel l=new JLabel("Ce bouton n'est pas valide ");
-        l.setFont(new Font("Calibri", Font.CENTER_BASELINE,15));
-        l.setForeground(Color.RED);
-        w.add(l,BorderLayout.CENTER);
-        fr.add(w,BorderLayout.CENTER);
-        
-     
-        fr.setVisible(true);
-	
         return false;
     }
 
